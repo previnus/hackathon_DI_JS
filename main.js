1b5238fac4b06366506b28042d05db7f1108f533
Previn - OXO <previn@oxo.mu>
2023-04-04 21:54:43 +0400

    added code for the background changer

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+/* BG COLOR CHANGER */
+
+//function to define a random color
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+//function to call the background div and set the color
+const setRandomBackground = () => {
+  var div = document.querySelector("body");
+  var color = getRandomColor();
+  div.style.transition = "background-color 2s ease-in-out";
+  div.style.backgroundColor = color;
+}
+
+//initialize background color change function
+setInterval(setRandomBackground, 3000);
+
+
+/* METAL PRICE */
+
 const getApi = () => {
     const url = 'https://api.metals.live/v1/spot';
     const corsUrl = 'https://corsproxy.io'; // have to make use of a proxy because of cors issues
